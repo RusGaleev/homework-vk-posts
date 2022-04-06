a0b780b71fd1a0e3a3738f8420e73151a89a0364
Rus Galeev <spyder44@yandex.ru>
2022-04-06 21:56:18 +0300

    make clone homework-vk-posts, make Repost.kt

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -1,32 +1,44 @@
-data class Post(
-    private var id: Long,
-    var ownerId: Long,
+open class Post(
+    open var id: Long,
+    open var ownerId: Long,
     val fromId: Long,
-    val createdBy: Long,
-    var date: Long,
+    open val createdBy: Long,
+    open var date: Long,
     val text: String,
     val replyPostId: Long,
     val friendsOnly: Boolean,
     val replyOwnerId: Long,
     val comments: Int,
-    val likes: Int,
+    likes: Int = 0,
+    val original: Post?,
     val reposts: Int,
     val postType: String,
     val singerId: String,
     val canPin: Boolean,
-    val canDelete: Boolean,
     val canEdit: Boolean,
     val markedAsAdds: Boolean,
-    val isFavorite: Boolean
+    val isFavorite: Boolean,
+    var copyHistory: Array<Post> = emptyArray<Post>()
 ) {
+    open var likes: Int = likes
+        set(value) {
+            if (value < 0) {
+                return
+            }
+            field = value
+        }
+        get() {
+            return field + (original?.likes ?: 0)
+        }
+
     object WallService {
         var posts = emptyArray<Post>()
-        //get() = field
+        get() = field
 
         fun add(post: Post): Post {
             //if (posts.lastIndex == -1) return throw IllegalArgumentException("Posts array is empty")
             posts += post
-            post.id = posts.lastIndex + 1L
+            //post.id = posts.lastIndex + 1L
             return posts.last()
         }
 
@@ -46,13 +58,36 @@ data class Post(
         fun likeById(id: Long) {
             for ((index, post) in posts.withIndex()) {
                 if (post.id == id) {
-                    posts[index] = post.copy(likes = post.likes + 1)
+                    posts[index].likes = posts[index].likes  + 1
                 }
             }
         }
         //TODO comments count
         //TODO reposts count
         //TODO post type. Type of the post, can be: post, copy, reply, postpone, suggest.
+    }
+
+    object postSource {
+        val type: String = ""
+        val platform: String = ""
+        val data: String = ""
+        val url: String = ""
+    }
+
+    object geo {
+        val type: String = ""
+        val coordinates: String = ""
+
+        object place {
+            val id: Long = 0
+            val title: String = ""
+            var latitude: Long = 0
+            val longitude: String = ""
+            val created: Long = 0
+            val icon: String = ""
+            val country: String = ""
+            val city: String = ""
+        }
 
     }
 }
